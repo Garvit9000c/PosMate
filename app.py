3789bf695c0fa3d3a7d0f3e3a770e89d390a1465
Slammer21 <65024708+Slammer21@users.noreply.github.com>
2021-05-24 18:39:39 +0530

    Name input part done and sql removed

--- app.py
+++ app.py
@@ -8,33 +8,20 @@ import base64
 from flask import send_from_directory
 from flask_bootstrap import Bootstrap
 # from AI import *
-from flaskext.mysql import MySQL
 # from datetime import datetime
 MEDIA_FOLDER= os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), 'data')
 
 app = Flask(__name__)
-  
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Subinsk284@'
-app.config['MYSQL_DB'] = 'flask'
- 
-mysql = MySQL(app)
-# date_created=db.Column(db.DateTime, default=datetime.utcnow)
-
+global name
 
 @app.route('/',methods=['GET','POST'])
 def login():
-   if request.method=='POST':
-      name = request.form['name']
-      
-      cursor = mysql.connection.cursor()
-      cursor.execute(''' INSERT INTO images VALUES(%s)''',(name))
-      mysql.connection.commit()
-      cursor.close()
-      return f"Done!!"
-   return render_template('home.html')
+        global name
+        if request.method=='POST':
+                name = request.form.get('name')
+                return render_template('camera.html')
+        return render_template('home.html')
    
 # @app.route("/image_info",methods= ['GET'])
 # def image_info():
@@ -52,9 +39,8 @@ def login():
 @app.route('/uploads/<path:filename>')
 def download_file(filename):
     return send_from_directory(MEDIA_FOLDER, filename, as_attachment=True)
-@app.route('/camera')
-def camera():
-    return render_template('camera.html')
+
     
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+    
