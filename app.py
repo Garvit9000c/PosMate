6eeded3251e9132737eb6645693dda3263186893
Subin <subinsk284@gmail.com>
2021-05-24 19:33:24 +0530

    requirements

--- app.py
+++ app.py
@@ -9,51 +9,20 @@ import yaml
 from flask import send_from_directory
 from flask_bootstrap import Bootstrap
 # from AI import *
-<<<<<<< HEAD
-from flaskext.mysql import MySQL
-from flask_mysqldb import MySQL
-=======
->>>>>>> 3e9b25d9af421fc854dcc57d9404058b41d1ed01
 # from datetime import datetime
 MEDIA_FOLDER= os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), 'data')
 
 app = Flask(__name__)
-<<<<<<< HEAD
-
-
-db=yaml.load(open('db.yaml'))
-
-
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
- 
-mysql = MySQL(app)
-=======
->>>>>>> 3e9b25d9af421fc854dcc57d9404058b41d1ed01
 
 global name
 
 @app.route('/',methods=['GET','POST'])
 def login():
-<<<<<<< HEAD
-   if request.method=='POST':
-    #   userDetails=request.form
-      name = request.form.get('name')      
-      cur = mysql.connection.cursor()
-      cur.execute(" INSERT INTO users (name) VALUES (%s)",(name,))
-      mysql.connection.commit()
-      cur.close()
-      return name
-   return render_template('home.html')
-=======
         global name
         if request.method=='POST':
                 name = request.form.get('name')
                 return render_template('camera.html')
         return render_template('home.html')
->>>>>>> 3e9b25d9af421fc854dcc57d9404058b41d1ed01
    
 # @app.route("/image_info",methods= ['GET'])
 # def image_info():
