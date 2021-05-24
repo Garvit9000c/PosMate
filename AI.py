ad5bc47bc5716b38c7b5769270abcf5393d576ea
Garvit9000c <909garvit@gmail.com>
2021-05-24 23:57:10 +0530

    Latest Stable Build

--- AI.py
+++ AI.py
@@ -1,14 +1,8 @@
+#Dependencies
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import tensorflow_hub as hub
-import numpy as np
-import cv2
-
-# Import matplotlib libraries
-from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
-import matplotlib.patches as patches
 
 # Dictionary that maps from joint names to keypoint indices.
 KEYPOINT_DICT = {
@@ -31,156 +25,21 @@ KEYPOINT_DICT = {
     'right_ankle': 16
 }
 
-# Maps bones to a matplotlib color name.
-KEYPOINT_EDGE_INDS_TO_COLOR = {
-    (0, 1): 'm',
-    (0, 2): 'c',
-    (1, 3): 'm',
-    (2, 4): 'c',
-    (0, 5): 'm',
-    (0, 6): 'c',
-    (5, 7): 'm',
-    (7, 9): 'm',
-    (6, 8): 'c',
-    (8, 10): 'c',
-    (5, 6): 'y',
-    (5, 11): 'm',
-    (6, 12): 'c',
-    (11, 12): 'y',
-    (11, 13): 'm',
-    (13, 15): 'm',
-    (12, 14): 'c',
-    (14, 16): 'c'
-}
-
-def _keypoints_and_edges_for_display(keypoints_with_scores,
-                                     height,
-                                     width,
-                                     keypoint_threshold=0.11):
-  keypoints_all = []
-  keypoint_edges_all = []
-  edge_colors = []
-  num_instances, _, _, _ = keypoints_with_scores.shape
-  for idx in range(num_instances):
-    kpts_x = keypoints_with_scores[0, idx, :, 1]
-    kpts_y = keypoints_with_scores[0, idx, :, 0]
-    kpts_scores = keypoints_with_scores[0, idx, :, 2]
-    kpts_absolute_xy = np.stack(
-        [width * np.array(kpts_x), height * np.array(kpts_y)], axis=-1)
-    kpts_above_thresh_absolute = kpts_absolute_xy[
-        kpts_scores > keypoint_threshold, :]
-    keypoints_all.append(kpts_above_thresh_absolute)
-
-    for edge_pair, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
-      if (kpts_scores[edge_pair[0]] > keypoint_threshold and
-          kpts_scores[edge_pair[1]] > keypoint_threshold):
-        x_start = kpts_absolute_xy[edge_pair[0], 0]
-        y_start = kpts_absolute_xy[edge_pair[0], 1]
-        x_end = kpts_absolute_xy[edge_pair[1], 0]
-        y_end = kpts_absolute_xy[edge_pair[1], 1]
-        line_seg = np.array([[x_start, y_start], [x_end, y_end]])
-        keypoint_edges_all.append(line_seg)
-        edge_colors.append(color)
-  if keypoints_all:
-    keypoints_xy = np.concatenate(keypoints_all, axis=0)
-  else:
-    keypoints_xy = np.zeros((0, 17, 2))
-
-  if keypoint_edges_all:
-    edges_xy = np.stack(keypoint_edges_all, axis=0)
-  else:
-    edges_xy = np.zeros((0, 2, 2))
-  return keypoints_xy, edges_xy, edge_colors
-
-
-def draw_prediction_on_image(
-    image, keypoints_with_scores, crop_region=None, close_figure=False,
-    output_image_height=None):
-  height, width, channel = image.shape
-  aspect_ratio = float(width) / height
-  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
-  # To remove the huge white borders
-  fig.tight_layout(pad=0)
-  ax.margins(0)
-  ax.set_yticklabels([])
-  ax.set_xticklabels([])
-  plt.axis('off')
-
-  im = ax.imshow(image)
-  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
-  ax.add_collection(line_segments)
-  # Turn off tick labels
-  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
-
-  (keypoint_locs, keypoint_edges,
-   edge_colors) = _keypoints_and_edges_for_display(
-       keypoints_with_scores, height, width)
-
-  line_segments.set_segments(keypoint_edges)
-  line_segments.set_color(edge_colors)
-  if keypoint_edges.shape[0]:
-    line_segments.set_segments(keypoint_edges)
-    line_segments.set_color(edge_colors)
-  if keypoint_locs.shape[0]:
-    scat.set_offsets(keypoint_locs)
-
-  if crop_region is not None:
-    xmin = max(crop_region['x_min'] * width, 0.0)
-    ymin = max(crop_region['y_min'] * height, 0.0)
-    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
-    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
-    rect = patches.Rectangle(
-        (xmin,ymin),rec_width,rec_height,
-        linewidth=1,edgecolor='b',facecolor='none')
-    ax.add_patch(rect)
-
-  fig.canvas.draw()
-  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-  image_from_plot = image_from_plot.reshape(
-      fig.canvas.get_width_height()[::-1] + (3,))
-  plt.close(fig)
-  if output_image_height is not None:
-    output_image_width = int(output_image_height / height * width)
-    image_from_plot = cv2.resize(
-        image_from_plot, dsize=(output_image_width, output_image_height),
-         interpolation=cv2.INTER_CUBIC)
-  return image_from_plot
-
-
-#module=tf.keras.models.load_model('./model')
+#Loading Model
 module = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/3")
 movenet=module.signatures['serving_default']
 input_size = 192
 
-def Dope(filename):
+#Function to Use Model
+def Coordinate(filename):
   image_path = filename
   image = tf.io.read_file(image_path)
   image = tf.image.decode_jpeg(image)
-
-  # Resize and pad the image to keep the aspect ratio and fit the expected size.
   input_image = tf.expand_dims(image, axis=0)
-  input_image = tf.cast(tf.image.resize_with_pad(
-      input_image, input_size, input_size), dtype=tf.int32)
-
-  # Run model inference.
+  input_image = tf.cast(tf.image.resize_with_pad(input_image, input_size, input_size), dtype=tf.int32)
   outputs = movenet(input_image)
-  # Output is a [1, 1, 17, 3] tensor.
   keypoint_with_scores = outputs['output_0']
   keypoints=[]
   for i in keypoint_with_scores[0][0]:
     keypoints.append([float(i[0]),float(i[1])])
-  #print(keypoints)
-
-  # Visualize the predictions with image.
-  display_image = tf.expand_dims(image, axis=0)
-  display_image = tf.cast(tf.image.resize_with_pad(
-      display_image, 1280, 1280), dtype=tf.int32)
-  output_overlay = draw_prediction_on_image(
-      np.squeeze(display_image.numpy(), axis=0), keypoint_with_scores)
-
-  plt.figure(figsize=(5, 5))
-  plt.imshow(output_overlay)
-  _ = plt.axis('off')
-  plt.savefig("./data/Processed.jpeg")
   return keypoints
-#print(Dope("./data/image.jpeg"))
--- app.py
+++ app.py
@@ -4,13 +4,10 @@ import PIL
 import io
 import os
 import base64
-import yaml
-# from config import MEDIA_FOLDER
-from flask import send_from_directory
 from flask_bootstrap import Bootstrap
-# from AI import *
-# from datetime import datetime
-MEDIA_FOLDER= os.path.join(os.path.dirname(os.path.dirname((os.path.abspath(__file__)))), 'data')
+from AI import *
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -24,22 +21,19 @@ def login():
                 return render_template('camera.html')
         return render_template('home.html')
    
-# @app.route("/image_info",methods= ['GET'])
-# def image_info():
-#     myfile= request.args.get('myimage').split(',')
-#     imgdata = base64.b64decode(myfile[1])
-#     im = Image.open(io.BytesIO(imgdata))
-#     filename="./data/image.jpeg"
-#     im.save(filename)
-#     width, height = im.size
-#     imgformat=im.format
-#     keypoints=Dope(filename)
-#     return jsonify(width=width,height=height,imgformat=imgformat)
+@app.route("/image_info",methods= ['GET'])
+def image_info():
+    myfile= request.args.get('myimage').split(',')
+    imgdata = base64.b64decode(myfile[1])
+    im = Image.open(io.BytesIO(imgdata))
+    filename="./data/image.jpeg"
+    im.save(filename)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    keypoints=Coordinate(filename)
+    print(keypoints)
+    return jsonify(x=0)
 
-
-@app.route('/uploads/<path:filename>')
-def download_file(filename):
-    return send_from_directory(MEDIA_FOLDER, filename, as_attachment=True)
 
     
 if __name__ == "__main__":
