e57dcde866e009ca8ccf17c150026897a2e74790
Garvit9000c <909garvit@gmail.com>
2021-05-25 21:08:25 +0530

    Alpha

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,7 +1,28 @@
 let subm = document.getElementById('subm');
 
+
+console.log(Notification.permission);
+
+if (Notification.permission === "granted" ){
+	showNotification(); 
+}else if (Notification.permission !=="denied"){ 
+	Notification.requestPermission().then( permission => {
+		if(permission== "granted"){
+			showNotification();
+		}
+	});
+}
+
+function showNotification() {
+	const notification = new Notification("New message incoming", { body: "Hi there. How are you doing?"
+	})
+}
+
+
 subm.addEventListener('click', () => {
     let loading=document.getElementById('loading');
     loading.style.display='block';
         
-    });
+});
+
+
