afbc6e2ffd8a3cf95564eedd0acb344bd4a0e217
Subin <subinsk284@gmail.com>
2021-05-27 01:05:11 +0530

    little change in navigation bar , added how it works

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import PIL
 import io
 import os
 import base64
-from AI import *
-from Logistic import *
+# from AI import *
+# from Logistic import *
 
 
 app = Flask(__name__)
@@ -53,36 +53,36 @@ def Monitoring():
     return render_template('Monitoring.html')
 
            
-@app.route("/image_info",methods= ['GET'])
-def image_info():
-    global data
-    myfile= request.args.get('myimage').split(',')
-    imgdata = base64.b64decode(myfile[1])
-    im = Image.open(io.BytesIO(imgdata))
-    filename="./data/image.jpeg"
-    im.save(filename)
-    keypoints=Coordinate(filename)
-    if data['flag']:
-        if len(data['train'])<10:
-            data['train'].append(keypoints)
-            return jsonify(x=0)
-        if len(data['train'])==10:
-            data['train']=trainCoordinates_process(data['train'])
-            return jsonify(x=1)  
-    flag,data['LSP']=btfunc(data['LSP'])
-    if flag==False:
-    	msg = Posture(data['train'], keypoints)
-    	if msg==0:
-    	    state,msg=0,None
-    	else:
-    	    state,msg=1,POSE_LABEL[msg]
-    if flag=='break':
-        state,msg=0,None
-    if flag=='start':
-        state,msg=1,POSE_LABEL[4] 
-    if flag==True:
-        state,msg=1,POSE_LABEL[5]   
-    return jsonify(state=state,msg=msg)
+# @app.route("/image_info",methods= ['GET'])
+# def image_info():
+#     global data
+#     myfile= request.args.get('myimage').split(',')
+#     imgdata = base64.b64decode(myfile[1])
+#     im = Image.open(io.BytesIO(imgdata))
+#     filename="./data/image.jpeg"
+#     im.save(filename)
+#     keypoints=Coordinate(filename)
+#     if data['flag']:
+#         if len(data['train'])<10:
+#             data['train'].append(keypoints)
+#             return jsonify(x=0)
+#         if len(data['train'])==10:
+#             data['train']=trainCoordinates_process(data['train'])
+#             return jsonify(x=1)  
+#     flag,data['LSP']=btfunc(data['LSP'])
+#     if flag==False:
+#     	msg = Posture(data['train'], keypoints)
+#     	if msg==0:
+#     	    state,msg=0,None
+#     	else:
+#     	    state,msg=1,POSE_LABEL[msg]
+#     if flag=='break':
+#         state,msg=0,None
+#     if flag=='start':
+#         state,msg=1,POSE_LABEL[4] 
+#     if flag==True:
+#         state,msg=1,POSE_LABEL[5]   
+#     return jsonify(state=state,msg=msg)
 
 
     
