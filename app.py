2f4878712f9c89be24d5cff28ecbe0a0308428bc
Subin <subinsk284@gmail.com>
2021-06-02 23:15:22 +0530

    hello

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask, render_template,jsonify,request,redirect,url_for
-<<<<<<< HEAD
-from Logistic import *
-
-app = Flask(__name__)
-
-global data
-data={ 'name':'Name', 'flag':False, 'train':[], 'test':[], 'LSP':None }
-=======
 import time
 app = Flask(__name__)
 
 global data
 data={ 'name':'Name', 'flag':False, 'train':[], 'test':[], 'LSP':None ,'time':time.time()}
->>>>>>> c9578aa33018d389d4ebeeec446c613563bb0336
 
 POSE_LABEL={
     0:'Correct',
@@ -24,102 +15,60 @@ POSE_LABEL={
 }
 
 @app.route('/',methods=['GET','POST'])
-<<<<<<< HEAD
-def login():
-    global data
-    if request.method=='POST':
-       data['name'] = request.form.get('login') 
-       return redirect(url_for('instructions'))
-=======
 def home():
     global data
     if request.method=='POST':
        data['name'] = request.form.get('login') 
        return redirect(url_for('work'))
->>>>>>> c9578aa33018d389d4ebeeec446c613563bb0336
     else:   
         return render_template('home.html')        
 
 
-<<<<<<< HEAD
-@app.route('/instructions',methods=['GET','POST'])
-def instructions():
-    if request.method=='POST':
-        return redirect('/Calibration')
-    return render_template('instructions.html')
-
-
-
-@app.route('/Calibration')
-def Calibration():
-    global data
-    data['flag']=True
-    data['train']=[]
-    return render_template('Calibration.html')
-
-@app.route('/Monitoring')
-def Monitoring():
-    global data
-    data['flag']=False
-    data['test']=[]
-    return render_template('Monitoring.html')
-
-           
-=======
 @app.route('/work')
 def work():
     return render_template('work.html')
 
 
 
->>>>>>> c9578aa33018d389d4ebeeec446c613563bb0336
-@app.route("/image_info",methods= ['GET'])
-def image_info():
-      global data
-      myfile= request.args.get('data')
-      keypoints=[]
-      Dict=eval(myfile)
-      for i in Dict:
-          x=i['position']['x']
-          x=x/640
-          y=i['position']['y']
-          y=y/480
-          keypoints.append([x,y])
-      if data['flag']:
-          if len(data['train'])<15:
-              data['train'].append(keypoints)
-              return jsonify(x=0)
-          if len(data['train'])==15:
-              data['train']=trainCoordinates_process(data['train'])
-              return jsonify(x=1)  
-      flag,data['LSP']=btfunc(data['LSP'])
-      if flag==False:
-      	msg = Posture(data['train'], keypoints)
-      	if msg==0:
-      	    state,msg=0,None
-      	else:
-<<<<<<< HEAD
-      	    state,msg=1,POSE_LABEL[msg]
-=======
-      	    if(time.time()-data['time'])<30:
-      	        state,msg=0,None
-      	    else:
-      	        state,msg=1,POSE_LABEL[msg]
-      	        data['time']=time.time()
->>>>>>> c9578aa33018d389d4ebeeec446c613563bb0336
-      if flag=='break':
-          state,msg=0,None
-      if flag=='start':
-          state,msg=1,POSE_LABEL[4] 
-      if flag==True:
-          state,msg=1,POSE_LABEL[5] 
-      return jsonify(state=state,msg=msg)
+# @app.route("/image_info",methods= ['GET'])
+# def image_info():
+#       global data
+#       myfile= request.args.get('data')
+#       keypoints=[]
+#       Dict=eval(myfile)
+#       for i in Dict:
+#           x=i['position']['x']
+#           x=x/640
+#           y=i['position']['y']
+#           y=y/480
+#           keypoints.append([x,y])
+#       if data['flag']:
+#           if len(data['train'])<15:
+#               data['train'].append(keypoints)
+#               return jsonify(x=0)
+#           if len(data['train'])==15:
+#               data['train']=trainCoordinates_process(data['train'])
+#               return jsonify(x=1)  
+#       flag,data['LSP']=btfunc(data['LSP'])
+#       if flag==False:
+#       	msg = Posture(data['train'], keypoints)
+#       	if msg==0:
+#       	    state,msg=0,None
+#       	else:
+#       	    if(time.time()-data['time'])<30:
+#       	        state,msg=0,None
+#       	    else:
+#       	        state,msg=1,POSE_LABEL[msg]
+#       	        data['time']=time.time()
+#       if flag=='break':
+#           state,msg=0,None
+#       if flag=='start':
+#           state,msg=1,POSE_LABEL[4] 
+#       if flag==True:
+#           state,msg=1,POSE_LABEL[5] 
+#       return jsonify(state=state,msg=msg)
 
 
     
 if __name__ == "__main__":
     app.run(debug=True)
-<<<<<<< HEAD
-    
-=======
->>>>>>> c9578aa33018d389d4ebeeec446c613563bb0336
