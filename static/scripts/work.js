356166d96136b26e49b1797289186437c232be48
Garvit9000c <909garvit@gmail.com>
2021-06-03 17:57:53 +0530

    Lightning

--- static/scripts/work.js
+++ static/scripts/work.js
@@ -74,18 +74,14 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 
 var myVar = setInterval(Pose, 100);
 let flag=true;
-document.getElementById("snap").addEventListener("click", function() {
+document.getElementById("button").addEventListener("click", function() {
 	if(flag){
 		clearInterval(myVar);
-		canvas.style.display = "none";
-		video.style.display = "block";
-		document.getElementById("snap").innerHTML = "Resume";
+		document.getElementById("text").innerHTML = "Resume";
 		flag=false;
 	}
 	else{
-		video.style.display = "none";
-		canvas.style.display = "block";
-		document.getElementById("snap").innerHTML = "Pause";
+		document.getElementById("text").innerHTML = "Pause";
 		myVar = setInterval(Pose, 100);
 		flag=true;
 	}
