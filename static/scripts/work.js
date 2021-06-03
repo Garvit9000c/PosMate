5c18a28baebefc3c78dee33b4fc688fbe1b0234c
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2021-06-03 21:16:51 +0530

    Update work.js

--- static/scripts/work.js
+++ static/scripts/work.js
@@ -10,44 +10,13 @@ async function load() {
 }  
 load(); 
 
-function resize(x) {
-  if (x.matches) { 
-    let constraint={video:{width: 320, height: 240}};
-    // var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    
-      canvas.width=320;
-      canvas.height=240;
-    
-    
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia(constraint).then(function(stream) {
-          video.srcObject = stream;
-          video.play();
-      });
-    }
-    
-  } else {
-    let constraint={video:{width: 640, height: 480}};
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-      canvas.width=480;
-      canvas.height=640;
-    }
-    
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia(constraint).then(function(stream) {
-          video.srcObject = stream;
-          video.play();
-      });
-    }
-  }
+
+let constraint={video:{width: 640, height: 480}};
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+  canvas.width=480;
+  canvas.height=640;
 }
-
-var x = window.matchMedia("(max-width: 600px)")
-resize(x) 
-x.addListener(resize)
-
-
 
 async function Pose() {
       const poses = await detect.estimatePoses(video);
