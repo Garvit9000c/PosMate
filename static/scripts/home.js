4b42797c94786f01dbc2b98c6241ff22aa5b5dc3
Subin <subinsk284@gmail.com>
2021-06-14 08:31:07 +0530

    iframe

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,12 +1,12 @@
-import anime from 'animejs/lib/anime.es.js';
 function validateForm() {
-    var x = document.forms["myForm"]["login"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  };
+  var x = document.forms["myForm"]["login"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+};
 
+import anime from 'animejs/lib/anime.es.js';
 const anime = require('animejs');
 anime({
   targets: 'div',
