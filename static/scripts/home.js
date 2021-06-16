626fdcc845f2c626b89b8355ec83efeafbb5b794
Subin <subinsk284@gmail.com>
2021-06-16 23:16:14 +0530

    mobile view

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,6 +1,6 @@
 function openNav() {
   $(".cbtn").html(`<img role="button" onclick="closeNav()" src="/static/close-button.png" alt="#">`);
-  $(".cbtn").css("display: block");
+  $(".cbtn").css({"display": "block"});
   $('.lpage').css("z-index:3");
   $('#mySidebar').css({"display":"flex","flex-direction":"column","height":"100%","width":"100%","align-items":"center","z-index":"4"})
 }
@@ -10,7 +10,7 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("mySidebar").style.height = "0";
   $('.lpage').css("z-index:6");
-  $(".cbtn").css("display: none");
+  $(".cbtn").css({"display": "none"});
   $('#mySidebar').css("display: none");
 }
 
